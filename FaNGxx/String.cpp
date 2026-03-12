4a588cd558d7ca6b05dd0821a806a77c019358c5
yxFaNGxy <1626287423@qq.com>
2026-03-12 15:22:01 +0800

    fix: 修复代码格式，取消注释并添加单词统计功能

--- FaNGxx/String.cpp
+++ FaNGxx/String.cpp
@@ -1,6 +1,6 @@
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
 // //P1125 [NOIP 2008 提高组] 笨小猴
 // bool judge(int x){//判断质数
@@ -36,3 +36,72 @@
 // }
 
 
+// int main(){
+//     string s;
+//     int n = 0;
+//     getline(cin, s);
+//     for(char c : s){
+//         if(c != ' ' && c != '\n') n++;
+//     }
+//     cout << n << endl;
+// }
+
+// int main(){
+//     string s,l;
+//     int q;//操作次数
+//     cin >> q >> s;
+//     for(int i = 0; i < q; i++){
+//         getline(cin, l);
+//         if(l[0] == '1'){
+//             string str = l.substr(2);
+//             str.insert(0, s);
+//             cout << str << endl;
+//         }
+//         else if(l[0] == '2'){
+//             s.substr(l.substr(2,1), l.substr(4,1))
+//             cout << s << endl;
+//         }
+//         else if(l[0] == '3'){
+            
+//         }
+//     }
+// }
+
+
+//P1308 [NOIP 2011 普及组] 统计单词数
+string s, t;
+int counts = 0;
+int sta = -1;
+string temp;
+
+int main(){
+	getline(cin, s);
+	getline(cin, t);
+	t += ' ';
+
+	for(char &c: s){
+		c = tolower(c);
+	} 
+	for(char &c: t){
+		c = tolower(c);
+	} 
+	
+	int len = s.size();
+	
+	for(int i = 0; i < t.size(); i++){
+		if(t[i] == ' '){
+			if(temp == s){
+				counts++;
+				if(sta == -1) sta = i - len;
+			}
+            temp = "";
+		}
+		else temp += t[i];
+	}
+	
+	if(sta != -1){
+		cout << counts << " " << sta << endl;
+	} 
+	else cout << "-1" << endl;
+	
+} 
