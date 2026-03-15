99860362e411582bd31b90716a575dd29671b649
yxFaNGxy <1626287423@qq.com>
2026-03-15 16:33:25 +0800

    P2437 蜜蜂路线

--- FaNGxx/dp.cpp
+++ FaNGxx/dp.cpp
@@ -137,27 +137,118 @@ using namespace std;
 //     cout << answer << endl;
 // }
 
-int f1(string s, vector<int>& dp){
-    int i;
-    int n = s.size();
+//记忆化搜索2
+// int f1(string s, vector<int>& dp){
+//     int i;
+//     int n = s.size();
 
-    dp[n] = 1;
-    for(i = n-1; i >= 0; i--){
-        if(s[i] == '0') dp[i] = 0;
-        else{
-            dp[i] = dp[i+1];
-            if(i+1 < n && ((s[i] - '0')*10 + (s[i+1] - '0')) <= 26){
-                dp[i] += dp[i+2];
-            }
-        }
-    }
-    return dp[0];
-}
+//     dp[n] = 1;
+//     for(i = n-1; i >= 0; i--){
+//         if(s[i] == '0') dp[i] = 0;
+//         else{
+//             dp[i] = dp[i+1];
+//             if(i+1 < n && ((s[i] - '0')*10 + (s[i+1] - '0')) <= 26){
+//                 dp[i] += dp[i+2];
+//             }
+//         }
+//     }
+//     return dp[0];
+// }
 
-int main(){
-    string s;
-    cin >> s;
-    vector<int> dp(s.size()+1);
-    int answer = f1(s, dp);
-    cout << answer << endl;
-}
+// int main(){
+//     string s;
+//     cin >> s;
+//     vector<int> dp(s.size()+1);
+//     int answer = f1(s, dp);
+//     cout << answer << endl;
+// }
+
+
+//P1028 [NOIP 2001 普及组] 数的计算
+
+// int f[1001] = {0};
+// int main(){
+//     int n;
+//     f[1] = 1;
+//     f[2] = 2;
+//     f[3] = 2;
+//     cin >> n;
+//     for(int i = 4; i <= n; i++){
+//         for(int j = 1; j <= i/2; j++){
+//             f[i] += f[j];
+//         }
+//         f[i]++;
+//     }
+
+//     cout << f[n] << endl;
+// }
+
+
+//P2437 蜜蜂路线
+//1
+// int main(){
+//     int m, n;
+//     cin >> m >> n;
+//     int i;
+//     int lastlast = 1;
+//     int last = 1;
+//     int cur;
+//     for( i = m+2; i <= n; i++){
+//         cur = lastlast + last;
+//         lastlast = last;
+//         last = cur;
+//     }
+
+//     cout << last << endl;
+// }
+
+//2
+// int find(int m, int n,  vector<int> dp){
+//     if(n == m) return 1;
+//     if(n == m+1) return 1;
+
+//     if(dp[n] != -1) return dp[n];
+//     int ans = find(m, n-1, dp) + find(m, n-2, dp);
+//     dp[n] = ans;
+//     return ans;
+// }
+// int main(){
+//     int m, n;
+//     cin >> m >> n;
+//     vector<int> dp(1001, -1);
+//     int ans = find(m, n, dp);
+//     cout << ans << endl;
+// }
+
+//3高精度版
+// const int N = 1000;
+// int a[N], b[N], ans[N];
+// int l, i, j;
+// int main(){
+//     int m, n;
+//     cin >> m >> n;
+//     a[m] = 1;
+//     b[m] = 1;
+//     l = m;
+
+//     for(i = m+2; i <= n; i++){
+//         for(j = m; j <= l; j++) ans[j] = a[j] + b[j];
+//         for(j = m; j <= l; j++){
+//             if(ans[j] >= 10){
+//                 ans[j] = ans[j]%10;
+//                 ++ans[j+1];
+//                 if(j == l) l++; 
+//             }
+//         }
+
+//         for(j = m; j <= l; j++){
+//             b[j] = a[j];
+//             a[j] = ans[j];
+            
+//         }
+//     }
+
+//     for(i = l; i >= m; i--){
+//         cout << ans[i];
+//     }
+// }
